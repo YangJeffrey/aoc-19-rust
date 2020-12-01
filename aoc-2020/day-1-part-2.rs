8ae6d70cebbca83f13b6230b1018467982f8e6bd
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2020-12-01 10:24:28 -0500

    Create day-1-part-2.rs

---
+++ aoc-2020/day-1-part-2.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let input = include_str!("day12020.txt");
+    let mut v: Vec<i32> = Vec::new();
+    let split = input.split("\n");
+    for s in split {
+        v.push(s.parse().unwrap());
+    }
+    let v_clone = v.clone();
+    let v_another_clone = v.clone();
+
+    for i in &v {
+        for j in &v_clone {
+            for k in &v_another_clone {
+                if i + j + k == 2020 {
+                    println!("{}", i * j * k);
+                }
+            }
+        }
+    }
+}
