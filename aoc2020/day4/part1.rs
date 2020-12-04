ae6cb606cfd4cb0022537055274191e19caec750
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2020-12-04 15:31:15 -0500

    Update part1.rs

--- aoc2020/day4/part1.rs
+++ aoc2020/day4/part1.rs
@@ -15,29 +15,21 @@ fn main() {
                 let mut isvalid = true;
                 if !str.contains("byr") {
                     isvalid = false;
-                    println!("{} is missing byr", str);
                 } else if !str.contains("iyr") {
                     isvalid = false;
-                    println!("{} is missing iyr", str);
                 } else if !str.contains("eyr") {
                     isvalid = false;
-                    println!("{} is missing eyr", str);
                 } else if !str.contains("hgt") {
                     isvalid = false;
-                    println!("{} is missing hgt", str);
                 } else if !str.contains("hcl") {
                     isvalid = false;
-                    println!("{} is missing hcl", str);
                 } else if !str.contains("ecl") {
                     isvalid = false;
-                    println!("{} is missing ecl", str);
                 } else if !str.contains("pid") {
                     isvalid = false;
-                    println!("{} is missing pid", str);
                 }
                 if isvalid == true {
                     validpasswords += 1;
-                    println!("{} is valid", str);
                 } else {
                     isvalid = true;
                 }
